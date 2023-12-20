21f761186e4c4bb7932b2cb3c127e9cc1ecc5974
Javierls2 <franciscoj.lorsan.1@educa.jcyl.es>
2023-12-20 20:25:06 +0100

    ultimo

--- app/vista/login.php
+++ app/vista/login.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    
-<!-- npx tailwindcss -i ./utiles/css/input.css -o ./utiles/css/dist/output.css --watch -->
+
+<!-- 
+    npm install -D tailwindcss
+    npx tailwindcss -i ./utiles/css/input.css -o ./utiles/css/dist/output.css --watch 
+-->
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PlayOn</title>
     <link rel="stylesheet" href="utiles/css/dist/output.css">
 </head>
-<body>
-    <div class="">
+<body class="container max-w-screen-2xl bg-black mx-auto">
+    <div class="container max-w-screen-xl bg-[url('../../img/Frame1.png')] mx-auto d">
+       <div class="max-w-52 bg-slate-400 h-11 mx-auto mt-10 my-[70px]"></div>
 
     </div>
     <div>
 
-    </div>
+    </div> 
+
 </body>
 </html>
